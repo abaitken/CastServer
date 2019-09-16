084b89c3109908bd0f6997093f5e0952d0d0a28a
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-09-16 23:29:26 +0100

    Setup static folder

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require("./browse")(app);
 require("./cast")(app);
 require("./playlist")(app);
 
+app.use(express.static('static'));
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
