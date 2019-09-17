9d20eec385c98771ad8c00be474a0b2f3768871b
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-09-17 12:56:56 +0100

    Implemented console key press detection to neatly bring down the server when running from a script

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ require("./playlist")(app);
 
 app.use(express.static('static'));
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+var listenPort = 3000;
+
+app.listen(listenPort, () => {
+  console.log("Server running on port " + listenPort);
 });
 
+// TODO : Conditionally execute this code when running as a service
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', function (str, key) {
+  if (key && key.name == 'q') process.exit();
+});
+
+console.log("Press 'q' to exit");
--- run.cmd
+++ run.cmd
@@ -3,5 +3,3 @@
 cd /d "%~dp0"
 call build.cmd
 call npm run server
-
-::TODO : introduce key press exit for server if additional commands are required
