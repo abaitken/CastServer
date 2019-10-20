08dc50680be4b8a5d0c5761f82b2016794a340f7
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-10-20 15:03:46 +0100

    Changed the rest service to return much simpler and consistent structures

--- browse.js
+++ browse.js
@@ -1,4 +1,57 @@
-function ProcessRequest(objectId, callback) {
+function RestructureContainer(item) {
+  
+  return {
+    id: item['$']['id'],
+    parentID: item['$']['parentID'],
+    childCount: item['$']['childCount'],
+    title: item['dc:title'],
+    class: item['upnp:class'],
+    // TODO : Can be an array!
+    albumArtURI: ['upnp:albumArtURI']['_']
+  };
+}
+
+function RestructureItem(item){
+  return {
+    id: item['$']['id'],
+    parentID: item['$']['parentID'],
+    title: item['dc:title'],
+    creator: item['dc:creator'],
+    artist: item['upnp:artist'],
+    album: item['upnp:album'],
+    genre: item['upnp:genre'],
+    // TODO : Can be an array! for images
+    res: item['res']['_'],
+    class: item['upnp:class'],
+    size: item['res']['$']['size'],
+    duration: item['res']['$']['duration'],
+    bitrate: item['res']['$']['bitrate'],
+    sampleFrequency: item['res']['$']['sampleFrequency'],
+    nrAudioChannels: item['res']['$']['nrAudioChannels'],
+    protocolInfo: item['res']['$']['protocolInfo'],
+    originalTrackNumber: item['upnp:originalTrackNumber'],
+    albumArtURI: ['upnp:albumArtURI']['_']
+  };
+}
+
+function RestructureEntities(o, redfineCallback) {
+  var result = [];
+
+  if (o) {
+    if (Array.isArray(o)) {
+      for (const key in o) {
+        var item = o[key];
+        result.push(redfineCallback(item));
+      }
+    }
+    else {
+      result.push(redfineCallback(o));
+    }
+  }
+  return result;
+}
+
+function FetchDNLAData(objectId, callback) {
   var request = require("request-promise-native");
 
   var requestBody =
@@ -34,7 +87,7 @@ function ProcessRequest(objectId, callback) {
   };
 
   request(requestOptions)
-    .then(function(body) {
+    .then(function (body) {
       var decode = require("unescape");
       const getResultRegex = /<Result>(?<DATA>[^]+)<\/Result>/;
       var match = getResultRegex.exec(body);
@@ -44,32 +97,76 @@ function ProcessRequest(objectId, callback) {
       var parser = new xml2js.Parser({ explicitArray: false });
 
       let jsonData;
-      parser.parseString(data, function(err, parseResult) {
+      parser.parseString(data, function (err, parseResult) {
         jsonData = parseResult["DIDL-Lite"];
       });
 
-      var result = {
-        container: jsonData["container"],
-        item: jsonData["item"]
-      };
-
-      callback(result);
+      callback(jsonData, null);
     })
-    .catch(function(error) {
-      console.log(error);
+    .catch(function (error) {
+      callback(null, error);
     });
 }
 
-module.exports = function(app) {
-  app.get("/browse/:id", (req, res, next) => {
-    ProcessRequest(req.params["id"], function(data) {
+function CreateErrorResult(error){
+  return {
+    error: "TODO : More error details"
+  };
+}
+
+function ProcessDNLAData(data)
+{  
+  var result = {
+    container: RestructureEntities(data["container"], RestructureContainer),
+    item: RestructureEntities(data["item"], RestructureItem)
+  };
+  return result;
+}
+
+module.exports = function (app) {
+
+  app.get("/raw/:id", (req, res, next) => {
+    FetchDNLAData(req.params["id"], function (data, error) {
+      if(error)// TODO : Is this correct, doesnt seem to work
+      {
+        res.json(CreateErrorResult(error));
+        return;
+      }
       res.json(data);
     });
   });
 
-  app.get("/browse", (req, res, next) => {
-    ProcessRequest("0", function(data) {
+  app.get("/raw", (req, res, next) => {
+    FetchDNLAData("0", function (data, error) {
+      if(error)// TODO : Is this correct, doesnt seem to work
+      {
+        res.json(CreateErrorResult(error));
+        return;
+      }
       res.json(data);
+    });
+  });
+
+
+  app.get("/browse/:id", (req, res, next) => {
+    FetchDNLAData(req.params["id"], function (data, error) {
+      if(error)// TODO : Is this correct, doesnt seem to work
+      {
+        res.json(CreateErrorResult(error));
+        return;
+      }
+      res.json(ProcessDNLAData(data));
+    });
+  });
+
+  app.get("/browse", (req, res, next) => {
+    FetchDNLAData("0", function (data, error) {
+      if(error)// TODO : Is this correct, doesnt seem to work
+      {
+        res.json(CreateErrorResult(error));
+        return;
+      }
+      res.json(ProcessDNLAData(data));
     });
   });
 
@@ -77,12 +174,21 @@ module.exports = function(app) {
     var lastPosition = req.params["id"].lastIndexOf("$");
     var parentId = req.params["id"].substr(0, lastPosition);
 
-    ProcessRequest(parentId, function(data) {
+    FetchDNLAData(parentId, function (dnlaData, error) {
+      
+      if(error)// TODO : Is this correct, doesnt seem to work
+      {
+        res.json(CreateErrorResult(error));
+        return;
+      }
+
+      var data = ProcessDNLAData(dnlaData);
+      
       if (data["item"]) {
         for (var index = 0; index < data["item"].length; index++) {
           var item = data["item"][index];
 
-          if (item["$"]["id"] == req.params["id"]) {
+          if (item["id"] == req.params["id"]) {
             res.json(item);
             return;
           }
@@ -91,7 +197,7 @@ module.exports = function(app) {
       if (data["container"]) {
         for (var index = 0; index < data["container"].length; index++) {
           var item = data["container"][index];
-          if (item["$"]["id"] == req.params["id"]) {
+          if (item["id"] == req.params["id"]) {
             res.json(item);
             return;
           }
