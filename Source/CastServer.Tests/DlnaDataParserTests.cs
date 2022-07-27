38d4b30b32c3d9159d0209c29d3528386bab2ad0
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-07-27 16:00:41 +0100

    feat: Improved test case

--- Source/CastServer.Tests/DlnaDataParserTests.cs
+++ Source/CastServer.Tests/DlnaDataParserTests.cs
@@ -15,6 +15,11 @@ namespace CastServer.Tests
             var xmlString = File.ReadAllText(@"TestData\TopLevelContainers.txt");
             var actual = DlnaDataParser.FromXmlString(xmlString).ToList();
             Assert.IsNotNull(actual);
+            Assert.AreEqual(4, actual.Count);
+            Assert.AreEqual("Browse Folders", actual[0].title);
+            Assert.AreEqual("Music", actual[1].title);
+            Assert.AreEqual("Pictures", actual[2].title);
+            Assert.AreEqual("Video", actual[3].title);
         }
     }
 }
